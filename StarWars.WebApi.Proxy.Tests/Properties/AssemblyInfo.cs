654c968fb12588ebad3cc670e985e65abbb8fdc3
Chet Gray <chet.gray@waystar.com>
2023-03-09 14:57:16 -0500

    Add project files.

---
+++ StarWars.WebApi.Proxy.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StarWars.WebApi.Proxy.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Waystar")]
+[assembly: AssemblyProduct("StarWars.WebApi.Proxy.Tests")]
+[assembly: AssemblyCopyright("Copyright © Waystar 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f9d8401e-5f15-4be9-99a1-ae01dfda2f21")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
